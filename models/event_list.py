75e7d6aa8d9742f59f4eaa4c42d9e49fa6ebb52a
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-28 15:40:13 +0100

    adding recurring boolean

--- models/event_list.py
+++ models/event_list.py
@@ -1,7 +1,7 @@
 from models.event import *
 
-event_1 = Event("31/10/2021", "Halloween Party", 25, "Grand Hall", "Scary, spooky night of fun")
-event_2 = Event("20/12/2021", "Christmas Feast", 100, "Rooftop Terrace", "Festive treats and fun for all")
+event_1 = Event("31/10/2021", "Halloween Party", 25, "Grand Hall", "Scary, spooky night of fun",True)
+event_2 = Event("20/12/2021", "Christmas Feast", 100, "Rooftop Terrace", "Festive treats and fun for all",False)
 
 events =[event_1,event_2]
 
