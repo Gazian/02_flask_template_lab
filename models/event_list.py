200958a99cbac0727540d647cc5e1bffc1a90e32
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-28 12:48:17 +0100

    commit to add lab setup

---
+++ models/event_list.py
@@ -0,0 +1,6 @@
+from models.event import *
+
+event_1 = Event("31/10/2021", "Halloween Party", 25, "Grand Hall", "Scary, spooky night of fun")
+event_2 = Event("20/12/2021", "Christmas Feast", 100, "Rooftop Terrace", "Festive treats and fun for all")
+
+events =[event_1,event_2]
